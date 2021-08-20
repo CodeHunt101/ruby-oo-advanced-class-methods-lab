4785c0d5c6ee140b2464eb49aa5715e89783a07c
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-20 18:31:59 +1000

    Lab completed

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if !!self.find_by_name(name)
+      self.find_by_name(name)
+    else self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort {|a,b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_info = filename.split(" - ")
+    song = Song.new
+    song.name = song_info[1].split(".")[0]
+    song.artist_name = song_info[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_info = filename.split(" - ")
+    song = Song.new
+    song.name = song_info[1].split(".")[0]
+    song.artist_name = song_info[0]
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
